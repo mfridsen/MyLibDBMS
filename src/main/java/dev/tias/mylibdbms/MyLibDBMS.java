01027f178fa454a8ef480d03ce8f552d38591117
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-06-25 05:03:10 +0200

    First commit

---
+++ src/main/java/dev/tias/mylibdbms/MyLibDBMS.java
@@ -0,0 +1,22 @@
+package dev.tias.mylibdbms;
+
+/**
+ * @author Mattias Fridsén
+ * @project ${PROJECT_NAME}
+ * @package dev.tias.mylibdbms
+ * @contact matfir-1@student.ltu.se
+ * @date ${DATE}
+ * <p>
+ * We plan as much as we can (based on the knowledge available),
+ * When we can (based on the time and resources available),
+ * But not before.
+ * <p>
+ * Brought to you by enough nicotine to kill a large horse.
+ */
+public class MyLibDBMS
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/dev/tias/mylibdbms/MyLibDBMSTest.java
@@ -0,0 +1,42 @@
+package dev.tias.mylibdbms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * @author Mattias Fridsén
+ * @project MyLibDBMS
+ * @date 6/25/2023
+ * @contact matfir-1@student.ltu.se
+ * <p>
+ * Unit Test for the MyLibDBMS class.
+ * <p>
+ * Brought to you by copious amounts of nicotine.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MyLibDBMSTest
+{
+    @BeforeAll
+    static void setUp()
+    {
+    }
+
+    @AfterAll
+    static void tearDown()
+    {
+    }
+
+    /**
+     *
+     */
+    @Test
+    @Order(1)
+    void testMyLibDBMS()
+    {
+        System.out.println("\n1: Testing MyLibDBMS...");
+        System.out.println("No test implemented here yet!");
+        //TODO Write your code here
+        System.out.println("\nTEST FINISHED.");
+    }
+}
